4176e12e7fe2e690ab72e19610cf8de6f1ba8927
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-03-03 13:24:14 +0900

    User ArrayObject.

--- src/Ray/Di/AbstractModule.php
+++ src/Ray/Di/AbstractModule.php
@@ -339,9 +339,9 @@ abstract class AbstractModule implements \ArrayAccess
      */
     public function install(AbstractModule $module)
     {
-        $this->bindings = array_merge_recursive((array)$this->bindings, (array)$module->bindings);
-        $this->pointcuts = array_merge_recursive((array)$module->pointcuts, (array)$module->pointcuts);
-        $this->container = array_merge_recursive((array)$module->container, (array)$module->container);
+        $this->bindings = new ArrayObject(array_merge_recursive((array)$this->bindings, (array)$module->bindings));
+        $this->pointcuts = new ArrayObject(array_merge_recursive((array)$module->pointcuts, (array)$module->pointcuts));
+        $this->container = new ArrayObject(array_merge_recursive((array)$module->container, (array)$module->container));
     }
 
     /**
