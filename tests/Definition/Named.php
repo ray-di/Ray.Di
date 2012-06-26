092f95484fef532062521e103d424c5e1e7905fa
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-06-26 21:56:59 +0900

    @Named annotation.

---
+++ tests/Definition/Named.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Ray\Di\Definition;
+
+use Ray\Di\Mock\DbInterface,
+    Ray\Di\Mock\UserInterface;
+
+class Named
+{
+    /**
+     * @var DbInterface
+     */
+    public $userDb;
+
+
+    /**
+     * @Inject
+     * @Named("user_db")
+     *
+     * @param DbInterface $db
+     *
+     */
+    public function setUserDb(DbInterface $db)
+    {
+        $this->userDb = $db;
+    }
+}
