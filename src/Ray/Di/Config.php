5852f6d945ee9d3adcd4a04236e940343318a008
koriym <akihito.koriyama@gmail.com>
2013-08-31 10:43:30 +0900

    rafactor: camelCase var name

--- src/Ray/Di/Config.php
+++ src/Ray/Di/Config.php
@@ -199,11 +199,11 @@ class Config implements ConfigInterface
 
         // fetch the values for parents so we can inherit them
         $parentClass = get_parent_class($class);
-        list($parent_params, $parent_setter, $parent_definition) =
+        list($parentParams, $parentSetter, $parentDefinition) =
         $parentClass ? $this->fetch($parentClass) : [$this->params['*'], $this->setter['*'], $this->annotation->getDefinition($class)];
         // stores the unified config and setter values
 
-        $unified_params = [];
+        $unifiedParams = [];
 
         // reflect on the class
         $classReflection = $this->getReflect($class);
@@ -218,35 +218,35 @@ class Config implements ConfigInterface
                 $explicit = $this->params->offsetExists($class) && isset($this->params[$class][$name]);
                 if ($explicit) {
                     // use the explicit value for this class
-                    $unified_params[$name] = $this->params[$class][$name];
+                    $unifiedParams[$name] = $this->params[$class][$name];
                     continue;
-                } elseif (isset($parent_params[$name])) {
+                } elseif (isset($parentParams[$name])) {
                     // use the implicit value for the parent class
-                    $unified_params[$name] = $parent_params[$name];
+                    $unifiedParams[$name] = $parentParams[$name];
                     continue;
                 } elseif ($param->isDefaultValueAvailable()) {
                     // use the external value from the constructor
-                    $unified_params[$name] = $param->getDefaultValue();
+                    $unifiedParams[$name] = $param->getDefaultValue();
                     continue;
                 }
                 // no value, use a null placeholder
-                $unified_params[$name] = null;
+                $unifiedParams[$name] = null;
             }
         }
         // merge the setters
-        $unified_setter = isset($this->setter[$class]) ? array_merge($parent_setter, $this->setter[$class]) : $parent_setter;
+        $unifiedSetter = isset($this->setter[$class]) ? array_merge($parentSetter, $this->setter[$class]) : $parentSetter;
 
         // merge the definitions
         $definition = isset($this->definition[$class]) ? $this->definition[$class] : $this->annotation->getDefinition($class);
-        /** @var $parent_definition \ArrayObject */
-        $defaults = array_merge($parent_definition->getArrayCopy(), $definition->getArrayCopy());
-        $unified_definition = new Definition($defaults);
-        $this->definition[$class] = $unified_definition;
+        /** @var $parentDefinition \ArrayObject */
+        $defaults = array_merge($parentDefinition->getArrayCopy(), $definition->getArrayCopy());
+        $unifiedDefinition = new Definition($defaults);
+        $this->definition[$class] = $unifiedDefinition;
 
         // done, return the unified values
-        $this->unified[$class][0] = $unified_params;
-        $this->unified[$class][1] = $unified_setter;
-        $this->unified[$class][2] = $unified_definition;
+        $this->unified[$class][0] = $unifiedParams;
+        $this->unified[$class][1] = $unifiedSetter;
+        $this->unified[$class][2] = $unifiedDefinition;
 
         return $this->unified[$class];
     }
