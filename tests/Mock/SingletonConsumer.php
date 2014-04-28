f1ecf7a1ef840fc45850928da5dd603ee693d06b
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-04-28 17:39:56 +0900

    add @Inject annotation

--- tests/Mock/SingletonConsumer.php
+++ tests/Mock/SingletonConsumer.php
@@ -1,12 +1,19 @@
 <?php
 namespace Ray\Di\Mock;
 
+use Ray\Di\Di\Inject;
+
 class SingletonConsumer {
 
     public static $instances = array();
 
     private $annotatedSingleton;
 
+    /**
+     * @param AnnotatedSingleton $annotatedSingleton
+     *
+     * @Inject
+     */
     public function __construct( AnnotatedSingleton $annotatedSingleton )
     {
         $this->annotatedSingleton = $annotatedSingleton;
