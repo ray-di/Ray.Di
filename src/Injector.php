5f4588260a3b5c0c7a2780a16f977e95840506b7
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-05-25 02:08:17 +0900

    gc

--- src/Injector.php
+++ src/Injector.php
@@ -392,34 +392,6 @@ class Injector implements InjectorInterface, \Serializable
         return [$method, $settings];
     }
 
-    function a(array $param, $key)
-    {
-        $annotate = $param[Definition::PARAM_ANNOTATE];
-        $typeHint = $param[Definition::PARAM_TYPEHINT];
-        $hasTypeHint = isset($this->module[$typeHint]) && isset($this->module[$typeHint][$annotate]) && ($this->module[$typeHint][$annotate] !== []);
-        $binding = $hasTypeHint ? $this->module[$typeHint][$annotate] : false;
-        $isNotBinding = $binding === false || isset($binding[AbstractModule::TO]) === false;
-        if ($isNotBinding && array_key_exists(Definition::DEFAULT_VAL, $param)) {
-            // default value
-            $param = $param[Definition::DEFAULT_VAL];
-
-            return $param;
-        }
-        if ($isNotBinding) {
-            // default binding by @ImplementedBy or @ProviderBy
-            $binding = $this->jitBinding($param, $typeHint, $annotate, $key);
-        }
-        list($bindingToType, $target) = $binding[AbstractModule::TO];
-
-        list($param, $bound) = $this->instanceBound($param, $bindingToType, $target, $binding);
-        if ($bound) {
-            return $param;
-        }
-
-        return $this->extractNotBoundParam($typeHint, $bindingToType, $target);
-    }
-
-
     /**
      * Extract parameter as defined
      *
