d63644530fde59b51e0f9a4da3f9c283939919c2
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-08-22 16:54:50 +0900

    fix typo

--- src/ModuleCacheInjector.php
+++ src/ModuleCacheInjector.php
@@ -1,11 +1,20 @@
 <?php
+/**
+ * This file is part of the Ray package.
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
+namespace Ray\Di;
+
+use Doctrine\Common\Cache\Cache;
+use Ray\Di\Module\ModuleCacheModule;
 
 class ModuleCacheInjector
 {
 
-    public static function create(callable $moduleProvider, Cache $cache, $cacheKey, $tmdDir)
+    public static function create(callable $moduleProvider, Cache $cache, $cacheKey, $tmpDir)
     {
-        $module = new ModuleCacheModule($moduleProvider, $cacheKey, $tmdDir);
+        $module = new ModuleCacheModule($moduleProvider, $cacheKey, $tmpDir);
         $injector = Injector::create([$module], $cache, $tmpDir)->enableBindCache();
 
         return $injector;
