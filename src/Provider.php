8ef5dbf83902e0b8b7b6ea6a406ef1eeed79b7c8
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-11-20 12:08:41 +0900

    [#87] add ProviderInterface alias interface

---
+++ src/Provider.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This file is part of the Ray package.
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
+namespace Ray\Di;
+
+/**
+ * Alias for ProviderInterface
+ */
+interface Provider extends ProviderInterface
+{
+}
