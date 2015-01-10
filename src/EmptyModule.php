9518ffdb468133383187f38549f890f19b07aae4
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-01-10 21:28:02 +0900

    add EmptyModule

---
+++ src/EmptyModule.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file is part of the Ray package.
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
+namespace Ray\Di;
+
+class EmptyModule extends AbstractModule
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+    }
+}
