3e4a631284e8cba768176284476c4ea4ae8459c6
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-05-25 16:59:54 +0900

    add red test

---
+++ tests/Fake/FakeUntarget.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of the _package_ package
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
+
+namespace Ray\Di;
+
+class FakeUntarget
+{
+    public $child;
+
+    public function __construct(FakeUntargetChild $child)
+    {
+        $this->child = $child;
+    }
+}
