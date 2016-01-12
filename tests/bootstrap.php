3626f304ab377d6bf4a998465002513693e9f3f2
Akihito Koriyama <akihito.koriyama@gmail.com>
2016-01-12 15:48:29 +0900

    update composer.json

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,6 @@
 <?php
 
-require dirname(__DIR__) . '/vendor/autoload.php';
-
+$loader = require dirname(__DIR__) . '/vendor/autoload.php';
+/* @var $loader \Composer\Autoload\ClassLoader */
+\Doctrine\Common\Annotations\AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 $_ENV['TMP_DIR'] = __DIR__ . '/tmp';
