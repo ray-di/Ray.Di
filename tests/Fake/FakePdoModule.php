25b94ee47803e23e61562fd5e389b81078e6b729
Akihito Koriyama <akihito.koriyama@gmail.com>
2016-02-29 19:18:05 +0900

    add red test

---
+++ tests/Fake/FakePdoModule.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ray\Di;
+
+class FakePdoModule extends AbstractModule
+{
+    protected function configure()
+    {
+        $this->bind(\PDO::class)->in(Scope::SINGLETON);
+        $this->bind(\PDO::class)->toConstructor(\PDO::class, 'dsn=pdo_dsn');
+        $this->bind()->annotatedWith('pdo_dsn')->toInstance('sqlite::memory:');
+    }
+}
