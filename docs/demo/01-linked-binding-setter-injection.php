95e89aea64f719f30247c7080ada56f53923d8a4
Akihito Koriyama <akihito.koriyama@gmail.com>
2016-02-29 13:14:00 +0900

    add setter injection demo

---
+++ docs/demo/01-linked-binding-setter-injection.php
@@ -0,0 +1,48 @@
+<?php
+
+use Ray\Di\AbstractModule;
+use Ray\Di\Di\Inject;
+use Ray\Di\Injector;
+
+require __DIR__.'/bootstrap.php';
+
+interface FinderInterface
+{
+}
+
+class Finder implements FinderInterface
+{
+}
+
+class FinderModule extends AbstractModule
+{
+    protected function configure()
+    {
+        $this->bind(FinderInterface::class)->to(Finder::class);
+        $this->bind(MovieListerInterface::class)->to(MovieLister::class);
+    }
+}
+
+interface MovieListerInterface
+{
+}
+
+class MovieLister implements MovieListerInterface
+{
+    public $finder;
+
+    /**
+     * @Inject
+     */
+    public function setFinder(FinderInterface $finder)
+    {
+        $this->finder = $finder;
+    }
+}
+
+$injector = new Injector(new FinderModule());
+$movieLister = $injector->getInstance(MovieListerInterface::class);
+/* @var $movieLister MovieLister */
+$works = ($movieLister->finder instanceof Finder);
+
+echo($works ? 'It works!' : 'It DOES NOT work!') . PHP_EOL;
