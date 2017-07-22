77a8b422b3a3146571db16748ef6ff906793f336
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-07-22 19:58:11 +0900

    add scalar type and return type

--- src/Name.php
+++ src/Name.php
@@ -29,22 +29,14 @@ final class Name
      */
     private $names;
 
-    /**
-     * @param string $name
-     */
-    public function __construct($name)
+    public function __construct(string $name = null)
     {
         if (! is_null($name)) {
             $this->setName($name);
         }
     }
 
-    /**
-     * @param \ReflectionParameter $parameter
-     *
-     * @return string
-     */
-    public function __invoke(\ReflectionParameter $parameter)
+    public function __invoke(\ReflectionParameter $parameter) : string
     {
         // single variable named binding
         if ($this->name) {
@@ -65,10 +57,7 @@ final class Name
         return self::ANY;
     }
 
-    /**
-     * @param string $name
-     */
-    private function setName($name)
+    private function setName(string $name) : void
     {
         // annotation
         if (class_exists($name, false)) {
@@ -91,7 +80,7 @@ final class Name
     /**
      * @param string $name
      */
-    private function parseName($name)
+    private function parseName(string $name) : void
     {
         $keyValues = explode(',', $name);
         foreach ($keyValues as $keyValue) {
