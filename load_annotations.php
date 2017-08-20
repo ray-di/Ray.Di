46f2408f8d9f9d3051f16d4df2450f0d83b2cd71
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-08-21 00:25:29 +0900

    add annotation loader script

---
+++ load_annotations.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * This file is part of the Ray.Di package.
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+require_once __DIR__ . '/src/Di/Inject.php';
+require_once __DIR__ . '/src/Di/Named.php';
+require_once __DIR__ . '/src/Di/PostConstruct.php';
+require_once __DIR__ . '/src/Di/Qualifier.php';
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,4 @@
  *
  * @license http://opensource.org/licenses/MIT MIT
  */
-$loader = require dirname(__DIR__) . '/vendor/autoload.php';
-/* @var $loader \Composer\Autoload\ClassLoader */
-\Doctrine\Common\Annotations\AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 $_ENV['TMP_DIR'] = __DIR__ . '/tmp';
