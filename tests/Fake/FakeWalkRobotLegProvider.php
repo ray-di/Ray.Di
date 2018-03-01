76f866dba24aed394ac84cae596927d743b0fb92
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-03-01 19:42:26 +0900

    fix phpdoc

--- tests/Fake/FakeWalkRobotLegProvider.php
+++ tests/Fake/FakeWalkRobotLegProvider.php
@@ -4,7 +4,7 @@ namespace Ray\Di;
 class FakeWalkRobotLegProvider implements ProviderInterface
 {
     /**
-     * @var InjectionPoint
+     * @var InjectionPointInterface
      */
     private $ip;
 
