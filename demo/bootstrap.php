d57424d93b3fa43a77a76ce62259450bc1e10f1f
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-03-02 05:47:13 +0900

    use 'class_exists' annotation load

--- demo/bootstrap.php
+++ demo/bootstrap.php
@@ -10,4 +10,4 @@ $loader = require dirname(__DIR__) . '/vendor/autoload.php';
 /* @var $loader \Composer\Autoload\ClassLoader */
 $loader->addPsr4('Ray\Di\Demo\\', __DIR__ . '/src/');
 // annotation loader
-AnnotationRegistry::registerLoader([$loader, 'loadClass']);
+AnnotationRegistry::registerLoader('class_exists');
