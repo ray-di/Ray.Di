bde216b56ea0bf87bbe16792672b8f64ad3b7e24
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-03-04 10:18:19 +0900

    soothe phpstan

--- tests/Fake/FakelNoConstructorCallModule.php
+++ tests/Fake/FakelNoConstructorCallModule.php
@@ -3,7 +3,7 @@ namespace Ray\Di;
 
 class FakelNoConstructorCallModule extends AbstractModule
 {
-    public function __construct(AbstractModule $module = null)
+    public function __construct()
     {
     }
 
