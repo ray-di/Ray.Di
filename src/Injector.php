e12dd8bd97398d9c97c3043c4428e103f4077774
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-03-04 11:12:59 +0900

    remove unnecessary type casting

--- src/Injector.php
+++ src/Injector.php
@@ -90,7 +90,7 @@ class Injector implements InjectorInterface
 
     private function setupClassDir(string $tmpDir = null) : string
     {
-        $tmpRootDir = is_dir((string) $tmpDir) ? $tmpDir : sys_get_temp_dir();
+        $tmpRootDir = is_dir($tmpDir) ? $tmpDir : sys_get_temp_dir();
         $classDir = $tmpRootDir . '/ray-di/';
         if (! is_dir($classDir)) {
             mkdir($classDir, 0777, true);
