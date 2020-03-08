8dbe4647962886e8a727c412e6199684c944d618
Akihito Koriyama <akihito.koriyama@gmail.com>
2020-03-08 12:48:43 +0900

    add more toConstructor demo

--- demo/05a-constructor-binding.php
+++ demo/05a-constructor-binding.php
@@ -9,17 +9,32 @@ use Ray\Di\Injector;
 
 // 	public PDO::__construct ( string $dsn [, string $username [, string $password [, array $options ]]] )
 
-class PdoModule extends AbstractModule
+class PdoModuleLegacyNaming extends AbstractModule
 {
     protected function configure()
     {
+        // dsn=pdo_dsn works
         $this->bind(PDO::class)->toConstructor(\PDO::class, 'dsn=pdo_dsn');
         $this->bind()->annotatedWith('pdo_dsn')->toInstance('sqlite::memory:');
     }
 }
 
-$injector = new Injector(new PdoModule);
+class PdoModule extends AbstractModule
+{
+    protected function configure()
+    {
+        // 'dsn' => 'pdo_dsn' works
+        $this->bind(PDO::class)->toConstructor(\PDO::class, ['dsn' => 'pdo_dsn']);
+        $this->bind()->annotatedWith('pdo_dsn')->toInstance('sqlite::memory:');
+    }
+}
+
+$injector = new Injector(new PdoModuleLegacyNaming);
 $pdo = $injector->getInstance(PDO::class);
 $works = $pdo instanceof PDO;
+
+$injector = new Injector(new PdoModule);
+$pdo = $injector->getInstance(PDO::class);
+$works &= $pdo instanceof PDO;
 
 echo($works ? 'It works!' : 'It DOES NOT work!') . PHP_EOL;
