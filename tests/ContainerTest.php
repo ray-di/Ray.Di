da99336289b688023884012d94b128f0bd6ac1d0
Akihito Koriyama <akihito.koriyama@gmail.com>
2020-04-29 16:05:28 +0900

    fix class name

--- tests/ContainerTest.php
+++ tests/ContainerTest.php
@@ -7,7 +7,6 @@ namespace Ray\Di;
 use BadMethodCallException;
 use PHPUnit\Framework\TestCase;
 use Ray\Aop\Matcher;
-use Ray\Aop\Pointcut;
 use Ray\Di\Exception\Unbound;
 
 class ContainerTest extends TestCase
