85805ef23df06c869ed5ac9480fdb276697d8577
Akihito Koriyama <akihito.koriyama@gmail.com>
2020-09-28 15:06:42 +0900

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,12 +3,16 @@
 declare(strict_types=1);
 
 $_ENV['TMP_DIR'] = __DIR__ . '/tmp';
+
 // cleanup tmp directory
-$unlink = function ($path) use (&$unlink) {
-    foreach ((array) glob(rtrim($path, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . '*') as $f) {
-        $file = (string) $f;
-        is_dir($file) ? $unlink($file) : unlink($file);
-        @rmdir($file);
-    }
-};
-$unlink($_ENV['TMP_DIR']);
+(function (): void {
+    $unlink = function (string $path) use (&$unlink): callable {
+        assert(is_callable($unlink));
+        foreach (array_filter((array) glob($path . '/*')) as $file) {
+            is_dir($file) ? $unlink($file) : unlink($file);
+            @rmdir($file);
+        }
+        return $unlink;
+    };
+    $unlink(__DIR__ . '/tmp')(__DIR__ . '/compiler/tmp');
+})();
