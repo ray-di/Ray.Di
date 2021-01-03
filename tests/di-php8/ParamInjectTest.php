1d8ca7373bc5d1f2462c8b8aafb3ec8ee8dd1e98
Akihito Koriyama <akihito.koriyama@gmail.com>
2021-01-03 12:30:37 +0900

    Update ParamInjectTest.php

--- tests/di-php8/ParamInjectTest.php
+++ tests/di-php8/ParamInjectTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Ray\Di;
 
 use PHPUnit\Framework\TestCase;
+use Ray\Di\Exception\MethodInvocationNotAvailable;
 
 class ParamInjectTest extends TestCase
 {
@@ -25,42 +26,42 @@ class ParamInjectTest extends TestCase
         $this->assertInstanceOf($expecetd, $assistedDependency);
     }
 
-//    public function testAssistedWithName(): void
-//    {
-//        $this->injector = new Injector(new FakeInstanceBindModule());
-//        $consumer = $this->injector->getInstance(FakeAssistedConsumer::class);
-//        /** @var FakeAssistedConsumer $consumer */
-//        $assistedDependency = $consumer->assistWithName('a7');
-//        $expecetd = 1;
-//        $this->assertSame($expecetd, $assistedDependency);
-//    }
-//
-//    public function testAssistedAnyWithName(): void
-//    {
-//        $injector = new Injector(new FakeToBindModule(new FakeInstanceBindModule()));
-//        $consumer = $injector->getInstance(FakeAssistedConsumer::class);
-//        /** @var FakeAssistedConsumer $consumer */
-//        [$assistedDependency1, $assistedDependency2] = $consumer->assistAny();
-//        $expected1 = 1;
-//        $this->assertSame($expected1, $assistedDependency1);
-//        $this->assertInstanceOf(FakeRobot::class, $assistedDependency2);
-//    }
-//
-//    public function testAssistedMethodInvocation(): void
-//    {
-//        $assistedConsumer = (new Injector(new FakeAssistedDbModule(), __DIR__ . '/tmp'))->getInstance(FakeAssistedParamsConsumer::class);
-//        /** @var FakeAssistedParamsConsumer $assistedConsumer */
-//        [$id, $db] = $assistedConsumer->getUser(1);
-//        /** @var FakeAbstractDb $db */
-//        $this->assertSame(1, $id);
-//        $this->assertSame(1, $db->dbId);
-//    }
-//
-//    public function testAssistedMethodInvocationNotAvailable(): void
-//    {
-//        $this->expectException(MethodInvocationNotAvailable::class);
-//        $assistedDbProvider = (new Injector(new FakeAssistedDbModule()))->getInstance(FakeAssistedDbProvider::class);
-//        /** @var FakeAssistedDbProvider $assistedDbProvider */
-//        $assistedDbProvider->get();
-//    }
+    public function testAssistedWithName(): void
+    {
+        $this->injector = new Injector(new FakeInstanceBindModule());
+        $consumer = $this->injector->getInstance(FakeAssistedConsumer::class);
+        /** @var FakeAssistedConsumer $consumer */
+        $assistedDependency = $consumer->assistWithName('a7');
+        $expecetd = 1;
+        $this->assertSame($expecetd, $assistedDependency);
+    }
+
+    public function testAssistedAnyWithName(): void
+    {
+        $injector = new Injector(new FakeToBindModule(new FakeInstanceBindModule()));
+        $consumer = $injector->getInstance(FakeAssistedConsumer::class);
+        /** @var FakeAssistedConsumer $consumer */
+        [$assistedDependency1, $assistedDependency2] = $consumer->assistAny();
+        $expected1 = 1;
+        $this->assertSame($expected1, $assistedDependency1);
+        $this->assertInstanceOf(FakeRobot::class, $assistedDependency2);
+    }
+
+    public function testAssistedMethodInvocation(): void
+    {
+        $assistedConsumer = (new Injector(new FakeAssistedDbModule(), __DIR__ . '/tmp'))->getInstance(FakeAssistedParamsConsumer::class);
+        /** @var FakeAssistedParamsConsumer $assistedConsumer */
+        [$id, $db] = $assistedConsumer->getUser(1);
+        /** @var FakeAbstractDb $db */
+        $this->assertSame(1, $id);
+        $this->assertSame(1, $db->dbId);
+    }
+
+    public function testAssistedMethodInvocationNotAvailable(): void
+    {
+        $this->expectException(MethodInvocationNotAvailable::class);
+        $assistedDbProvider = (new Injector(new FakeAssistedDbModule()))->getInstance(FakeAssistedDbProvider::class);
+        /** @var FakeAssistedDbProvider $assistedDbProvider */
+        $assistedDbProvider->get();
+    }
 }
