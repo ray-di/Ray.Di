1c3f97748f2b3de9b6f8f4d12bb03e216aaf28da
Akihito Koriyama <akihito.koriyama@gmail.com>
2022-02-27 11:23:41 +0900

    fixup! Import doc comment from Guice wiki

--- src/di/InjectorInterface.php
+++ src/di/InjectorInterface.php
@@ -4,6 +4,13 @@ declare(strict_types=1);
 
 namespace Ray\Di;
 
+/**
+ * Builds the graphs of objects that make up your application
+ *
+ * The injector tracks the dependencies for each type and uses bindings to inject them.
+ * This is the core of Ray.Di, although you rarely interact with it directly.
+ * This "behind-the-scenes" operation is what distinguishes dependency injection from its cousin, the service locator pattern.
+ */
 interface InjectorInterface
 {
     /**
@@ -19,8 +26,6 @@ interface InjectorInterface
      * @phpstan-return mixed
      *
      * @psalm-template T of object
-     *
-     * @see https://github.com/ray-di/Ray.Di
      */
     public function getInstance($interface, $name = Name::ANY);  // @phpstan-ignore-line
 }
