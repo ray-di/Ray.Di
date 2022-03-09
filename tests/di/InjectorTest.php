277b6b1490f6a8199abc4545be78135db5bbaa51
Akihito Koriyama <akihito.koriyama@gmail.com>
2022-03-09 19:26:20 +0900

    Soothe SA

--- tests/di/InjectorTest.php
+++ tests/di/InjectorTest.php
@@ -490,7 +490,5 @@ class InjectorTest extends TestCase
         assert($fakeSet instanceof FakeSet);
         $this->assertInstanceOf(ProviderInterface::class, $fakeSet->provider);
         $this->assertInstanceOf(FakeEngine::class, $fakeSet->provider->get());
-        $this->assertInstanceOf(ProviderInterface::class, $fakeSet->engineProvider);
-        $this->assertInstanceOf(FakeEngineInterface::class, $fakeSet->engineProvider->get());
     }
 }
